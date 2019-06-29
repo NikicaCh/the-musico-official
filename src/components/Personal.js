c26696b1f83c6bd1e0acd734b6c84d32ca318d0b
Nikica Maksimovski <nikica_chelsea@hotmail.com>
2019-06-29 11:15:49 +0200

    working with playlists

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -12,17 +12,30 @@ class Personal extends React.Component {
         }
         this.playlistDetails = this.playlistDetails.bind(this);
         this.closeDetails = this.closeDetails.bind(this);
+        this.clickUnhoverable = this.clickUnhoverable.bind(this);
     }
-
-    playlistDetails = () => {
+    clickUnhoverable = (e) => {
+        if(!$(e.target).hasClass("hoverable") && $(e.target).hasClass("personal-item")) {
+            $(".personal-item").removeClass("item-unselected")
+            $(".personal-item").removeClass("item-selected")
+            $(e.target).addClass("item-selected")
+        }
+    }
+    playlistDetails = (e) => {
         $('.personal-details').addClass("maximized")
         $(".personal-item").removeClass("hoverable")
         $(".close-personal").removeClass("hide")
+        console.log("playlist id:",e.target.id)
+        $(".personal-item").removeClass("item-selected")
+        $(".personal-item").removeClass("item-unselected")
+        $(e.target).parent().addClass("item-selected")
     }
     closeDetails = () => {
         $('.personal-details').removeClass("maximized")
         $(".personal-item").addClass("hoverable")
         $(".close-personal").addClass("hide")
+        $(".personal-item").removeClass("item-selected")
+        $(".personal-item").addClass("item-unselected")
     }
     componentDidMount() {
         let token = accessToken();
@@ -35,7 +48,10 @@ class Personal extends React.Component {
                         backgroundImage: `url(${item.images[0].url})`
                     }
                     return (
-                        <div className="personal-item hoverable" style={style}><img src={require("../icons/personal-arrow.webp")} className="personal-arrow" onClick={this.playlistDetails}></img></div>
+                        <div className="personal-item hoverable" style={style} onClick={this.clickUnhoverable}>
+                        <img id={item.id} src={require("../icons/personal-arrow.webp")} className="personal-arrow" onClick={this.playlistDetails}></img>
+                        <img src={require("../icons/play-playlist.webp")} className="play-playlist"></img>
+                        </div>
                     )
                 })
                 this.setState({arrayOfFeaturingPlaylists})
@@ -54,11 +70,14 @@ class Personal extends React.Component {
                     {this.state.arrayOfFeaturingPlaylists}
                 </div>
                 <div className="personal-details">
-                                <img
-                                className="close-personal hide"
-                                src={require("../icons/pill-close.png")}
-                                onClick={this.closeDetails}>
-                            </img></div>
+                    <img
+                    className="close-personal hide"
+                    src={require("../icons/pill-close.png")}
+                    onClick={this.closeDetails}>
+                    </img>
+                    <h1>Everyday favourites</h1>
+                    <span>2014</span>
+                </div>
                 <div className="personal-section">
                     <div className="personal-item hoverable"><img src={require("../icons/personal-arrow.webp")} className="personal-arrow"></img></div>
                     <div className="personal-item hoverable"><img src={require("../icons/personal-arrow.webp")} className="personal-arrow"></img></div>
